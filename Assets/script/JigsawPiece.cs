c83d9b9738f6af24823ad79657fe352d26c56196
MathildeStan <mathilde.stanborough@gmail.com>
2023-04-26 12:31:29 +0200

    æoshdæfohsdoæfh

---
+++ Assets/script/JigsawPiece.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+
+public class JigsawPiece : MonoBehaviour
+{
+    // Reference to the puzzle script
+    private JigsawPuzzle jigsawPuzzle;
+
+    // Store the original rotation of the piece
+    private Quaternion originalRotation;
+
+    // Flag to indicate if the piece is in the correct position
+    private bool isCorrectlyPlaced = false;
+
+    // Initialize the reference to the puzzle script and store the original rotation of the piece
+    private void Start()
+    {
+        Shuffle();
+
+        jigsawPuzzle = transform.parent.GetComponent<JigsawPuzzle>();
+        originalRotation = transform.localRotation;
+    }
+
+    // Shuffle the puzzle pieces randomly by rotating them
+    private void Shuffle()
+    {
+        float randomAngle = 90 * Random.Range(1, 4);
+        transform.Rotate(new Vector3(0f, 0f, randomAngle));
+    }
+
+    // Check for left mouse button click to rotate the piece and check if the puzzle is complete
+    private void OnMouseDown()
+    {
+        if (!jigsawPuzzle.puzzleComplete)
+        {
+            transform.localRotation *= Quaternion.Euler(new Vector3(0f, 0f, -90f));
+            jigsawPuzzle.CheckPuzzleComplete();
+        }
+    }
+
+    // Reset the piece to its original rotation
+    public void ResetPiece()
+    {
+        isCorrectlyPlaced = false;
+        transform.localRotation = originalRotation;
+    }
+
+    // Set the flag to indicate that the piece is in the correct position
+    public void SetCorrectlyPlaced()
+    {
+        isCorrectlyPlaced = true;
+    }
+
+    // Check if the piece is in the correct position
+    public bool IsCorrectlyPlaced()
+    {
+        return isCorrectlyPlaced;
+    }
+}
---
+++ Assets/script/JigsawPuzzle.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class JigsawPuzzle : MonoBehaviour
+{
+    // Name of the scene to load when the puzzle is complete
+    public string nextSceneName;
+
+    // Flag to indicate if the puzzle is complete
+    public bool puzzleComplete = false;
+
+    // Check for touch input to load the next scene once the puzzle is complete
+    private void Update()
+    {
+        if (puzzleComplete && Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            SceneManager.LoadScene(nextSceneName);
+        }
+    }
+
+    // Check if all puzzle pieces are correctly placed
+    public void CheckPuzzleComplete()
+    {
+        bool allPiecesCorrectlyPlaced = true;
+        foreach (Transform child in transform)
+        {
+            if (child.localEulerAngles != Vector3.zero)
+            {
+                // If any puzzle piece is not correctly placed, set the flag to false and exit the loop
+                allPiecesCorrectlyPlaced = false;
+                break;
+            }
+        }
+
+        // If all puzzle pieces are correctly placed, set the puzzleComplete flag to true
+        if (allPiecesCorrectlyPlaced)
+        {
+            puzzleComplete = true;
+            Debug.Log("Puzzle complete!");
+        }
+    }
+}
