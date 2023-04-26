71d472664d3f85f0caa338d5d88ed82ff425f549
MathildeStan <mathilde.stanborough@gmail.com>
2023-04-26 13:29:03 +0200

    Soundsmanager

---
+++ Assets/script/SoundManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager Instance;
+
+    [SerializeField] private AudioSource _musicSouce;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
