419c9633952468fcbc976c19e37f770f9a5236cd
Kasper <Kasperbnbn@live.dk>
2023-05-06 22:57:51 +0200

    Update DragAndDrop.cs

--- Assets/script/Drag And Drop/DragAndDrop.cs
+++ Assets/script/Drag And Drop/DragAndDrop.cs
@@ -1,30 +1,37 @@
 using UnityEngine.EventSystems;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class DragAndDrop : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
 {
     public RectTransform correctPosition;
     public float snapDistance = 10f;
+    public float returnSpeed = 5f;
 
     public float Alp = .6f;
     public float Alph = 1;
 
     private RectTransform rectTransform;
     private CanvasGroup canvasGroup;
+    private Vector2 startPosition;
+    private bool isDragging = false;
+
     //Gets the components
     private void Awake()
     {
         rectTransform = GetComponent<RectTransform>();
         canvasGroup = GetComponent<CanvasGroup>();
+        startPosition = rectTransform.anchoredPosition;
     }
-    //https://docs.unity3d.com/2018.2/Documentation/ScriptReference/EventSystems.IDragHandler.html
+
     //Method to start the drag
     public void OnBeginDrag(PointerEventData eventData)
     {
         canvasGroup.alpha = Alp;
         //Block other images, so you can drag over the images.
         canvasGroup.blocksRaycasts = true;
+        isDragging = true;
     }
 
     //Method during the dragging
@@ -42,8 +49,6 @@ public class DragAndDrop : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndD
         float distance = Vector2.Distance(rectTransform.anchoredPosition, correctPosition.anchoredPosition);
 
         // If the distance is within the snap distance, snap the image to the correct position
-
-        //https://docs.unity3d.com/ScriptReference/RectTransform-anchoredPosition.html
         if (distance <= snapDistance)
         {
             rectTransform.anchoredPosition = correctPosition.anchoredPosition;
@@ -61,11 +66,36 @@ public class DragAndDrop : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndD
             // Switches scene if all images are on their selceted spot.
             if (numCorrect == FindObjectsOfType<DragAndDrop>().Length)
             {
-               
                 SceneManager.LoadScene(3);
             }
         }
+        else
+        {
+            // If the object is not placed correctly, return it to its original position
+            isDragging = false;
+            StartCoroutine(ReturnToStart());
+        }
 
         canvasGroup.blocksRaycasts = true;
+    }
+
+    // Coroutine to return the object to its original position
+    private IEnumerator ReturnToStart()
+    {
+        while (Vector2.Distance(rectTransform.anchoredPosition, startPosition) > 0.1f)
+        {
+            rectTransform.anchoredPosition = Vector2.Lerp(rectTransform.anchoredPosition, startPosition, returnSpeed * Time.deltaTime);
+            yield return null;
+        }
+        rectTransform.anchoredPosition = startPosition;
+    }
+
+    // Stop the return coroutine if the object is being dragged again
+    private void Update()
+    {
+        if (isDragging)
+        {
+            StopCoroutine(ReturnToStart());
+        }
     }
 }
